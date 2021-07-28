18bb6b56971127965be291192b79dd3ebdfff0d5
paulsplanet <71366478+paulsplanet@users.noreply.github.com>
2021-07-27 21:34:38 -0700

    router practice 4

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import About from "./About";
 import Home from "./Home";
-import { Link, Route } from "react-router-dom";
+import { Link, Route, Switch } from "react-router-dom";
 import Profiles from "./Profiles";
 import HistorySample from "./HistorySample";
 
@@ -15,10 +15,19 @@ const App = () => {
         <li><Link to="/history">History</Link></li>
       </ul>
       <hr />
-      <Route path="/" component={Home} exact={true} />
-      <Route path="/about" component={About} />
-      <Route path="/profiles" component={Profiles} />
-      <Route path="/history" component={HistorySample} />
+      <Switch>
+        <Route path="/" component={Home} exact={true} />
+        <Route path="/about" component={About} />
+        <Route path="/profiles" component={Profiles} />
+        <Route path="/history" component={HistorySample} />
+        <Route render={({ location }) => (
+          <div>
+            <h2>This page doesn't exist.</h2>
+            <p>{location.pathname}</p>
+          </div>
+        )} />
+      </Switch>
+      
     </div>
   )
 }
--- src/Profiles.js
+++ src/Profiles.js
@@ -1,19 +1,26 @@
 import React from "react";
-import { Link, Route } from "react-router-dom";
+import { NavLink, Route } from "react-router-dom";
 import Profile from "./Profile";
+import WithRouterSample from "./WithRouterSample";
 
 
 const Profiles = () => {
+    const activeStyle = {
+        background: "black",
+        color: "white",
+    }
     return(
         <div>
             <h3>User List</h3>
             <ul>
-                <li><Link to="/profiles/velopert">Velopert</Link></li>
-                <li><Link to="/profiles/gildong">Gildong</Link></li>
+                <li><NavLink activeStyle={activeStyle} to="/profiles/velopert">Velopert</NavLink></li>
+                <li><NavLink activeStyle={activeStyle} to="/profiles/gildong">Gildong</NavLink></li>
             </ul>
 
             <Route path="/profiles" exact render={() => <div>Select User</div>} />
             <Route path="/profiles/:username" component={Profile} />
+
+            <WithRouterSample />
         </div>
 
     )  
